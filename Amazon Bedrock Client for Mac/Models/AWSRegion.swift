9679adb85cdb320dcc133ef57cd834c37e7810c9
Sanghwa Na <sanghwa@amazon.com>
2024-04-24 15:31:05 +0900

    Add more regions.

--- Amazon Bedrock Client for Mac/Models/AWSRegion.swift
+++ Amazon Bedrock Client for Mac/Models/AWSRegion.swift
@@ -13,8 +13,10 @@ enum AWSRegion: String, CaseIterable, Identifiable {
     case usEast1 = "us-east-1"
     case usWest2 = "us-west-2"
     case apSouthEast1 = "ap-southeast-1"
+    case apSouthEast2 = "ap-southeast-2"
     case apNortheEast1 = "ap-northeast-1"
     case euCentral1 = "eu-central-1"
+    case euWest3 = "eu-west-3"
     
     // ... add other regions
 }
